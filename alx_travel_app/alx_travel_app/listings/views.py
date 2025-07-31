3e43e5edc4c58082ab971dfdfaeae05f34e6dfbe
elainees-devs <elainees.devs@gmail.com>
2025-07-31 18:13:26 +0300

    Add basic ListingViewSet and BookingViewSet with full CRUD support

--- alx_travel_app/alx_travel_app/listings/views.py
+++ alx_travel_app/alx_travel_app/listings/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from .models import Listing, ListingSerializer, Booking, BookingSerializer
 
 
 class SampleView(APIView):
     def get(self, request):
         return Response({"message": "Hello from listings API!"})
+
+
+class ListingViewSet(ModelViewSet):
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
+
+    
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
 
